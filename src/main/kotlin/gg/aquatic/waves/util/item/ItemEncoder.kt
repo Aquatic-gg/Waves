973b79acf39ea986f22c7ccab9c0de199e576b67
MrLarkyy_ <studiotadikl@gmail.com>
2025-04-22 17:13:58 +0200

    Refactor ItemEncoder to simplify item serialization logic.
    Replaced try-catch blocks with streamlined helper methods for encoding and decoding items. This improves readability and reduces boilerplate code by leveraging ItemStack utility functions directly.

--- src/main/kotlin/gg/aquatic/waves/util/item/ItemEncoder.kt
+++ src/main/kotlin/gg/aquatic/waves/util/item/ItemEncoder.kt
@@ -6,38 +6,21 @@ import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder
 object ItemEncoder {
 
     fun encode(itemStack: ItemStack): String {
-        return String(itemStack.serializeAsBytes(), Charsets.UTF_8)
+        return String(itemStack.serializeAsBytes())
     }
 
     fun decode(base64: String): ItemStack {
         return ItemStack.deserializeBytes(Base64Coder.decode(base64))
     }
 
-    /*
+
     fun encodeItems(items: List<ItemStack>): String {
-        try {
-            ByteArrayOutputStream().use { outputStream ->
-                BukkitObjectOutputStream(outputStream).use { dataOutput ->
-                    dataOutput.writeObject(items)
-                    return String(Base64Coder.encode(outputStream.toByteArray()))
-                }
-            }
-        } catch (exception: Exception) {
-            throw exception
-        }
+        return String(ItemStack.serializeItemsAsBytes(items))
     }
 
     fun decodeItems(base64: String): List<ItemStack> {
-        try {
-            ByteArrayInputStream(Base64Coder.decode(base64)).use { inputStream ->
-                BukkitObjectInputStream(inputStream).use { dataInput ->
-                    return (dataInput.readObject() as List<ItemStack>)
-                }
-            }
-        } catch (exception: Exception) {
-            throw exception
-        }
+        return ItemStack.deserializeItemsFromBytes(Base64Coder.decode(base64)).toList()
     }
-     */
+
 
 }
