4f9da9c30e318b2305562dd24f72aa8eab99b062
MrLarkyy_ <studiotadikl@gmail.com>
2025-04-22 17:12:23 +0200

    Refactor ItemEncoder to simplify encode/decode logic
    Replaced custom ByteArray streams with `serializeAsBytes` and `deserializeBytes` for cleaner and more efficient item (de)serialization. Removed redundant exception handling and unused imports for improved readability. Commented out unused bulk encode method for potential future use.

--- src/main/kotlin/gg/aquatic/waves/util/item/ItemEncoder.kt
+++ src/main/kotlin/gg/aquatic/waves/util/item/ItemEncoder.kt
@@ -1,39 +1,19 @@
 package gg.aquatic.waves.util.item
 
 import org.bukkit.inventory.ItemStack
-import org.bukkit.util.io.BukkitObjectInputStream
-import org.bukkit.util.io.BukkitObjectOutputStream
 import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 
 object ItemEncoder {
 
     fun encode(itemStack: ItemStack): String {
-        try {
-            ByteArrayOutputStream().use { outputStream ->
-                BukkitObjectOutputStream(outputStream).use { dataOutput ->
-                    dataOutput.writeObject(itemStack)
-                    return String(Base64Coder.encode(outputStream.toByteArray()))
-                }
-            }
-        } catch (exception: Exception) {
-            throw exception
-        }
+        return String(itemStack.serializeAsBytes(), Charsets.UTF_8)
     }
 
     fun decode(base64: String): ItemStack {
-        try {
-            ByteArrayInputStream(Base64Coder.decode(base64)).use { inputStream ->
-                BukkitObjectInputStream(inputStream).use { dataInput ->
-                    return (dataInput.readObject() as ItemStack)
-                }
-            }
-        } catch (exception: Exception) {
-            throw exception
-        }
+        return ItemStack.deserializeBytes(Base64Coder.decode(base64))
     }
 
+    /*
     fun encodeItems(items: List<ItemStack>): String {
         try {
             ByteArrayOutputStream().use { outputStream ->
@@ -58,5 +38,6 @@ object ItemEncoder {
             throw exception
         }
     }
+     */
 
 }
