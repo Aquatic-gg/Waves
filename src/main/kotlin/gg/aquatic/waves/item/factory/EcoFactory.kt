b86a83338c1e4e91fee418d9f8fa7ff0fd924781
MrLarkyy_ <studiotadikl@gmail.com>
2025-10-16 11:34:31 +0200

    Handle `EmptyTestableItem` in `EcoFactory` creation logic to ensure null is returned for invalid item lookups.

--- src/main/kotlin/gg/aquatic/waves/item/factory/EcoFactory.kt
+++ src/main/kotlin/gg/aquatic/waves/item/factory/EcoFactory.kt
@@ -1,11 +1,16 @@
 package gg.aquatic.waves.item.factory
 
 import com.willfp.eco.core.items.Items
+import com.willfp.eco.core.recipe.parts.EmptyTestableItem
 import gg.aquatic.waves.item.ItemHandler
 import org.bukkit.inventory.ItemStack
 
 object EcoFactory: ItemHandler.Factory {
     override fun create(id: String): ItemStack? {
-        return Items.lookup(id).item
+        val lookup = Items.lookup(id)
+        if (lookup is EmptyTestableItem) {
+            return null
+        }
+        return lookup.item
     }
 }
