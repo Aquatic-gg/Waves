e070ef9faf41fc8d3ccb1eafe00cd4b654f7a9f0
MrLarkyy_ <studiotadikl@gmail.com>
2025-11-15 09:13:24 +0100

    Remove unused player interaction messages and clean up commented-out event handling code in `MEGInteractableHandler` to improve code clarity.

--- src/main/kotlin/gg/aquatic/waves/interactable/MEGInteractableHandler.kt
+++ src/main/kotlin/gg/aquatic/waves/interactable/MEGInteractableHandler.kt
@@ -14,7 +14,6 @@ class MEGInteractableHandler {
             val interactable = base.interactable
             if (it.slot == EquipmentSlot.OFF_HAND) return@event
             if (it.action == Action.INTERACT_ON) return@event
-            it.player.sendMessage("You have interacted!")
             val event = InteractableInteractEvent(
                 interactable,
                 it.player,
@@ -22,27 +21,5 @@ class MEGInteractableHandler {
             )
             interactable.onInteract(event)
         }
-
-        /*
-        event<PlayerChunkLoadEvent> {
-            val chunkKey = it.chunk.chunkId()
-            for (interactable in megInteractables[chunkKey] ?: return@event) {
-                if (interactable.location.world != it.player.world) continue
-                if (interactable.audience.canBeApplied(it.player)) {
-                    interactable.addViewer(it.player)
-                }
-            }
-        }
-
-        event<PlayerChunkUnloadEvent> {
-            val chunkKey = it.chunk.chunkId()
-            for (interactable in megInteractables[chunkKey] ?: return@event) {
-                if (interactable.location.world != it.player.world) continue
-                if (interactable.audience.canBeApplied(it.player)) {
-                    interactable.removeViewer(it.player)
-                }
-            }
-        }
-         */
     }
 }
